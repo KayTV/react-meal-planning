ea70762f6179befaf6bf4451b1328c321563d75e
KaylynTV39 <kaylynvannorstrand@navapbc.com>
2024-07-10 16:33:04 -0400

    fruit button

--- src/app/buttons/generate-fruit-button.tsx
+++ src/app/buttons/generate-fruit-button.tsx
@@ -1,12 +1,26 @@
 "use client";
 import React from "react";
 import { Button, Card, Col, Row } from "react-bootstrap";
+import { fruit } from "../../lib/fruit";
+
+const fruitProvided: string[] = fruit;
 
 export default function FruitButton() {
+  let fruitForTheWeek: string[] = [];
+
+  const generateFruit = () => {
+    fruitForTheWeek = [];
+    while (fruitForTheWeek.length !== 2) {
+      const randomObject =
+        fruitProvided[Math.floor(Math.random() * fruitProvided.length)];
+      fruitForTheWeek.push(randomObject);
+    }
+  };
+
   return (
     <Row>
       <Col md={3}>
-        <Button type="button" onClick={generateMeals} variant="warning">
+        <Button type="button" onClick={generateFruit} variant="warning">
           Generate Meal Plan
         </Button>
       </Col>
